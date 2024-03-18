9a870bb2b7daa9ec66ec1f6e9eb77d270dba0367
Nicholas Croulet <ncroulet@gmail.com>
2024-03-18 02:23:29 -0400

    Added buttons and onClickListeners for start/join session.

--- app/src/main/java/edu/temple/bseek/MainActivity.kt
+++ app/src/main/java/edu/temple/bseek/MainActivity.kt
@@ -2,10 +2,26 @@ package edu.temple.bseek
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var createSessionButton: Button
+    lateinit var joinSessionButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        createSessionButton = findViewById(R.id.startSessionButton)
+        joinSessionButton = findViewById(R.id.joinSessionButton)
+
+        createSessionButton.setOnClickListener {
+
+        }
+
+        joinSessionButton.setOnClickListener {
+
+        }
+
     }
 }
