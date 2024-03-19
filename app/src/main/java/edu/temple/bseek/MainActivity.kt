b549f10caea6d6dac31c915f6ef17e2361056013
Nicholas Croulet <ncroulet@gmail.com>
2024-03-19 17:44:08 -0400

    Added sensor manager with listener to MainActivity for testing

--- app/src/main/java/edu/temple/bseek/MainActivity.kt
+++ app/src/main/java/edu/temple/bseek/MainActivity.kt
@@ -1,19 +1,58 @@
 package edu.temple.bseek
 
+import android.Manifest
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var createSessionButton: Button
     lateinit var joinSessionButton: Button
 
+    // create vars for sensorManager, light sensor and sensorEventListener
+    lateinit var sensorManager: SensorManager
+    var lightSensor: Sensor? = null
+    lateinit var sensorEventListener: SensorEventListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // get light sensor
+        sensorManager = getSystemService(SensorManager::class.java)
+        lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
+
+
+        // sensor event listener for ambient light sensor
+        sensorEventListener = object : SensorEventListener {
+            override fun onSensorChanged(p0: SensorEvent?) {
+                p0?.run {
+                    if (p0.sensor.type == Sensor.TYPE_LIGHT) {
+                        Log.d("Light Sensor Event", p0.values[0].toString())
+                    }
+                }
+
+            }
+
+            override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+                TODO("Not yet implemented")
+            }
+
+        }
+
+        // register for sensor updates(will persist as long as the activity does)
+        sensorManager.registerListener(sensorEventListener, lightSensor, SensorManager.SENSOR_DELAY_FASTEST)
 
         createSessionButton = findViewById(R.id.startSessionButton)
         joinSessionButton = findViewById(R.id.joinSessionButton)
@@ -25,6 +64,8 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+
+
         createSessionButton.setOnClickListener {
             // start a session using an intent
         }
@@ -33,5 +74,12 @@ class MainActivity : AppCompatActivity() {
             // prompt the user for a session key
         }
 
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        // un register listener for light sensor
+        sensorManager.unregisterListener(sensorEventListener)
     }
 }
